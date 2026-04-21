3f0d96114df95b68b1e32d29fa0bfdbb03ad2a98
ZimEthane <ethane.zim@hotmail.com>
2026-04-21 10:38:37 +0200

    feat: add comments to NoSQL scripts for better clarity

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
-"""Provides stats about Nginx logs stored in MongoDB"""
+"""Script that provides stats about Nginx logs stored in MongoDB."""
 
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
+def main():
+    """Display statistics about Nginx logs."""
+    client = MongoClient("mongodb://127.0.0.1:27017")
     collection = client.logs.nginx
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    total = collection.count_documents({})
-    print(f"{total} logs")
-
+    print("{} logs".format(collection.count_documents({})))
     print("Methods:")
-    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+    for method in methods:
+        print("\tmethod {}: {}".format(
+            method, collection.count_documents({"method": method})
+        ))
+    print("{} status check".format(
+        collection.count_documents({"method": "GET", "path": "/status"})
+    ))
 
-    status = collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status} status check")
+
+if __name__ == "__main__":
+    main()
